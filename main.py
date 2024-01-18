c9410c1446ec5837515413f1fc3be0c1cc05a149
Ayush Paliwal <paliwal.ayush721@gmail.com>
2024-01-18 14:09:27 +0530

    Used OOPS to handle different clients and bind the methods and attributes together

--- main.py
+++ main.py
@@ -1,98 +1,116 @@
-from utils import bhav_copy_equities
+from utils import nse_utils
 from json2html import *
 import datetime
 import boto3
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+class nse_report():
 
-def send_email(email_message):
-    """Send email."""
-    message = MIMEMultipart()
-    message['Subject'] = 'Daily Portfolio Report'
-    message['From'] = 'paliwal.ayush721@gmail.com'
-    message['To'] = 'paliwal.ayush721@gmail.com'
+    def __init__(self,dict,email):
+        self.dict = dict
+        self.email = email
 
-    part = MIMEText(email_message, 'html')
-    message.attach(part)
+    def send_email(email_message,email):
+        """Send email."""
+        message = MIMEMultipart()
+        message['Subject'] = 'Daily Portfolio Report'
+        part = MIMEText(email_message, 'html')
+        message.attach(part)
 
-    email_client = boto3.client('ses', region_name='us-east-1')
-    try:
-        email_client.send_raw_email(
-            Source='paliwal.ayush721@gmail.com',
-            Destinations=['paliwal.ayush721@gmail.com'],
-            RawMessage={
-                'Data': message.as_string()
-            }
-        )
-        print("Email Sent Successfully")
-    except Exception:
-        print(Exception)
-
-
-def nse_data():
-    """Get data From NSE for the specified Date"""
-    date = (datetime.datetime.today()-datetime.timedelta(days=1)).strftime('%d-%m-%Y')
-    data = bhav_copy_equities(date)
-    return data
+        email_client = boto3.client('ses', region_name='us-east-1')
+        try:
+            email_client.send_raw_email(
+                Source='paliwal.ayush721@gmail.com',
+                Destinations=[email],
+                RawMessage={
+                    'Data': message.as_string()
+                }
+            )
+            print(f"Email Sent Successfully to {email}")
+        except Exception:
+            print(Exception)
 
 
-def filter_data(data,share):
-    """Filter Through The data to get the data for the required Shares"""
-    filter = (data['SYMBOL']== share['Name'])
-    data = data.loc[filter,['SYMBOL','CLOSE','PREVCLOSE']]
-    data = (str(data).replace('\n',' ')).split()
-    
-    symbol = data[4]
-    close = float(data[5])
-    prevClose = float(data[6])
-    quantity = share['Quantity']
-    buying_price = share['Buying Price']
-
-    final_dict = {
-        "Symbol" : symbol,
-        "Close" : close,
-        "PrevClose" : prevClose,
-        "Change" : round(close - prevClose,2),
-        "Change (%)" : round((close-prevClose)/prevClose*100,2),
-        "Day's Gain" : round((close - prevClose)*quantity,2),
-        "Quantity" :quantity,
-        "Buying Price" : buying_price,
-        'Net Profit' : round((close-buying_price)*quantity,2),
-        "Return(%)" : round(buying_price/close*100,2)
-    }
-    return final_dict
+    def nse_data():
+        """Get data From NSE for the specified Date"""
+        date = (datetime.datetime.today()).strftime('%d-%m-%Y')
+        # date = (datetime.datetime.today()-datetime.timedelta(days=1)).strftime('%d-%m-%Y')
+        data = nse_utils.bhav_copy_equities(date)
+        return data
 
 
-def html_table(json_data):
-    """Convert The generated JSON into HTML Table"""
-    table = json2html.convert(json_data, table_attributes = 'border="0.5" style="background-color:black;"')
-    table = table.replace('<thead>','<thead align="Centre" style="background-color:limegreen;">')
-    table = table.replace('<tbody>','<tbody align="Right" style="background-color:chartreuse;">')
-    table = table.replace('<th>','<th width="100">')
-    return table
+    def filter_data(data,share):
+        """Filter Through The data to get the data for the required Shares"""
+        filter = (data['SYMBOL']== share['Name'])
+        data = data.loc[filter,['SYMBOL','CLOSE','PREVCLOSE']]
+        data = (str(data).replace('\n',' ')).split()
+        
+        symbol = data[4]
+        close = float(data[5])
+        prevClose = float(data[6])
+        quantity = share['Quantity']
+        buying_price = share['Buying Price']
+
+        final_dict = {
+            "Symbol" : symbol,
+            "Close" : close,
+            "PrevClose" : prevClose,
+            "Change" : round(close - prevClose,2),
+            "Change (%)" : round((close-prevClose)/prevClose*100,2),
+            "Day's Gain" : round((close - prevClose)*quantity,2),
+            "Quantity" :quantity,
+            "Buying Price" : buying_price,
+            'Net Profit' : round((close-buying_price)*quantity,2),
+            "Return(%)" : round(buying_price/close*100,2)
+        }
+        return final_dict
+
+
+    def html_table(json_data):
+        """Convert The generated JSON into HTML Table"""
+        table = json2html.convert(json_data, table_attributes = 'border="0.5" style="background-color:black;"')
+        table = table.replace('<thead>','<thead align="Centre" style="background-color:limegreen;">')
+        table = table.replace('<tbody>','<tbody align="Right" style="background-color:chartreuse;">')
+        table = table.replace('<th>','<th width="100">')
+        return table
+
+
+    def send_report(self):
+        final_data = []
+        try:
+            data = nse_report.nse_data()
+        except Exception:
+            raise ValueError("NO DATA FOUND")
+        else:
+            for item in self.dict:
+                dict_data = nse_report.filter_data(data,item)
+                final_data.append(dict_data)
+            table = nse_report.html_table(final_data)  
+
+            nse_report.send_email(email_message=table,email=self.email)
 
 
 def main(event,context):
     """Main Function"""
-    final_data = []
-    dict = [{'Name' :'ADANIENT','Quantity': 100,'Buying Price' : 2000},
+    
+    #Report for Ayush
+    ayush_share = [{'Name' :'ADANIENT','Quantity': 100,'Buying Price' : 2000},
             {'Name' :'IDEAFORGE','Quantity': 100,'Buying Price' : 650},
             {'Name' :'IRFC','Quantity': 2000,'Buying Price' : 65},
             {'Name' :'KPIGREEN','Quantity': 100,'Buying Price' : 800}]
-
-    try:
-        data = nse_data()
-    except Exception:
-        raise ValueError("NO DATA FOUND")
-    else:
-        for item in dict:
-            dict_data = filter_data(data,item)
-            final_data.append(dict_data)
-        table = html_table(final_data)  
-
-        send_email(email_message=table)
     
+    ayush = nse_report(dict=ayush_share,email="paliwal.ayush721@gmail.com")
+    ayush.send_report()
+
+    #report for client2
+    client2_share = [{'Name' :'RELIANCE','Quantity': 100,'Buying Price' : 2000},
+            {'Name' :'SUNPHARMA','Quantity': 100,'Buying Price' : 650},
+            {'Name' :'TATAMOTORS','Quantity': 2000,'Buying Price' : 65},
+            {'Name' :'ASIANPAINT','Quantity': 100,'Buying Price' : 800}]
+
+    client2 = nse_report(dict=client2_share,email="apaliwal326@gmail.com")
+    client2.send_report()
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -17,31 +17,32 @@ header = {
     }
 dd_mm_yyyy = '%d-%m-%Y'
 
-def nse_urlfetch(url):
-    r_session = requests.session()
-    res = r_session.get(url, headers=header)
-    return res
+class nse_utils:
+    def nse_urlfetch(url):
+        r_session = requests.session()
+        res = r_session.get(url, headers=header)
+        return res
 
 
-def bhav_copy_equities(trade_date: str):
-    """
-    get nse bhav copy as per the traded date provided
-    :param trade_date:
-    :return: pandas dataframe
-    """
-    trade_date = datetime.strptime(trade_date, dd_mm_yyyy)
-    url = 'https://archives.nseindia.com/content/historical/EQUITIES/'
-    payload = f"{str(trade_date.strftime('%Y'))}/{str(trade_date.strftime('%b').upper())}/" \
-              f"cm{str(trade_date.strftime('%d%b%Y').upper())}bhav.csv.zip"
-    request_bhav = nse_urlfetch(url + payload)
-    bhav_df = pd.DataFrame()
-    if request_bhav.status_code == 200:
-        zip_bhav = zipfile.ZipFile(BytesIO(request_bhav.content), 'r')
-        for file_name in zip_bhav.filelist:
-            if file_name:
-                bhav_df = pd.read_csv(zip_bhav.open(file_name))
-    elif request_bhav.status_code == 403:
-        raise FileNotFoundError(f' Data not found, change the date...')
-    bhav_df = bhav_df[['SYMBOL', 'SERIES', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'LAST', 'PREVCLOSE', 'TOTTRDQTY',
-                       'TOTTRDVAL', 'TIMESTAMP', 'TOTALTRADES']]
-    return bhav_df
+    def bhav_copy_equities(trade_date: str):
+        """
+        get nse bhav copy as per the traded date provided
+        :param trade_date:
+        :return: pandas dataframe
+        """
+        trade_date = datetime.strptime(trade_date, dd_mm_yyyy)
+        url = 'https://archives.nseindia.com/content/historical/EQUITIES/'
+        payload = f"{str(trade_date.strftime('%Y'))}/{str(trade_date.strftime('%b').upper())}/" \
+                f"cm{str(trade_date.strftime('%d%b%Y').upper())}bhav.csv.zip"
+        request_bhav = nse_utils.nse_urlfetch(url + payload)
+        bhav_df = pd.DataFrame()
+        if request_bhav.status_code == 200:
+            zip_bhav = zipfile.ZipFile(BytesIO(request_bhav.content), 'r')
+            for file_name in zip_bhav.filelist:
+                if file_name:
+                    bhav_df = pd.read_csv(zip_bhav.open(file_name))
+        elif request_bhav.status_code == 403:
+            raise FileNotFoundError(f' Data not found, change the date...')
+        bhav_df = bhav_df[['SYMBOL', 'SERIES', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'LAST', 'PREVCLOSE', 'TOTTRDQTY',
+                        'TOTTRDVAL', 'TIMESTAMP', 'TOTALTRADES']]
+        return bhav_df
